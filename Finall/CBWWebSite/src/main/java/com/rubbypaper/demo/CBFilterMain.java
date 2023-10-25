a10e9ffa9b06ffbd67a28cf8214e9b60bb03fa87
junhyeoklim <gur710@gmail.com>
2023-10-25 16:36:56 +0900

    index 수정 및 필터 기능 연결 구현 중

---
+++ Finall/CBWWebSite/src/main/java/com/rubbypaper/demo/CBFilterMain.java
@@ -0,0 +1,99 @@
+package com.rubbypaper.demo;
+
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+import com.madgag.gif.fmsware.GifDecoder;
+import com.rubbypaper.demo.CBFilterSimulation.ColorBlindType;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CBFilterMain {
+    public static void main(String[] args) {
+        // 콘솔에서 이미지 파일 경로와 색맹 유형을 입력받습니다.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("이미지 파일 경로를 입력하세요");
+        String imagePath = scanner.nextLine();
+        System.out.println("색맹 유형을 선택하세요");
+        int colorBlindTypeIndex = scanner.nextInt();
+        scanner.close();
+
+        // 입력된 색맹 유형에 해당하는 열거형 상수를 얻습니다.
+        ColorBlindType colorBlindType = getColorBlindTypeByIndex(colorBlindTypeIndex);
+
+        try {
+            // 이미지 파일을 읽어옵니다.
+            File imageFile = new File(imagePath);
+
+            // GIF 파일인지 확인
+            if (imageFile.getName().toLowerCase().endsWith(".gif")) {
+                // GIF 파일 디코딩
+                GifDecoder decoder = new GifDecoder();
+                decoder.read(new FileInputStream(imageFile));
+
+                List<BufferedImage> frames = new ArrayList<>();
+
+                for (int i = 0; i < decoder.getFrameCount(); i++) {
+                    BufferedImage frame = decoder.getFrame(i);
+                    BufferedImage simulatedFrame = CBFilterSimulation.simulateColorBlindness(frame, colorBlindType);
+                    frames.add(simulatedFrame);
+                }
+
+                // 새로운 GIF 이미지 생성
+                String outputImagePath = "src/main/resources/Images/Converted_" + imageFile.getName();
+                createGifFromFrames(frames, outputImagePath);
+
+                System.out.println("GIF 이미지 변환이 완료되었습니다.");
+            } else {
+                // GIF가 아닌 경우 일반 이미지 변환
+                BufferedImage originalImage = ImageIO.read(imageFile);
+
+                // 색맹 시뮬레이션을 적용한 이미지 생성
+                BufferedImage simulatedImage = CBFilterSimulation.simulateColorBlindness(originalImage, colorBlindType);
+
+                // 이미지 저장
+                String outputImagePath = "src/main/resources/Images/Converted_" + imageFile.getName();
+                ImageIO.write(simulatedImage, "png", new File(outputImagePath));
+
+                System.out.println("이미지 변환이 완료되었습니다.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 숫자로 입력된 색맹 유형을 열거형 상수로 변환
+    private static ColorBlindType getColorBlindTypeByIndex(int index) {
+        switch (index) {
+            case 1:
+                return ColorBlindType.PROTANOPIA; //적색맹
+            case 2:
+                return ColorBlindType.DEUTERANOPIA; //녹색맹
+            case 3:
+                return ColorBlindType.TRITANOPIA; //청색맹
+            default:
+                return ColorBlindType.PROTANOPIA; // 기본값은 적색맹
+        }
+    }
+
+    // 디코딩된 프레임들을 이용하여 GIF 이미지 생성
+    private static void createGifFromFrames(List<BufferedImage> frames, String outputPath) {
+        AnimatedGifEncoder encoder = new AnimatedGifEncoder();
+
+        // 출력 경로 설정
+        encoder.start(outputPath);
+        encoder.setRepeat(0); // 반복 설정 (0은 무한 반복)
+
+        for (BufferedImage frame : frames) {
+            encoder.setDelay(100); // 각 프레임 간의 딜레이 (100ms)
+            encoder.addFrame(frame);
+        }
+
+        encoder.finish();
+    }
+}
